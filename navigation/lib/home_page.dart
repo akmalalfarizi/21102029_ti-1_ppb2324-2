ce3cbdb48756c6479dc8bdbf3c25e0ff2fa28650
akmalalfarizi <alfariziakmal2482@gmail.com>
2024-03-23 02:29:00 +0700

    all

---
+++ navigation/lib/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/second_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SecondPage(
+                        data: 'Data dari Home Page',
+                      ),
+                    ),
+                  );
+                },
+                child: const Text('Menuju halaman kedua'),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                child: Text(''),
+              ), 
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/second_page',
+                      arguments: 'Menggunakan route');
+                },
+                child: const Text('Menuju halaman kedua dengan route'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
